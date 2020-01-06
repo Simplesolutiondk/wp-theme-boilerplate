07a7ff388106e67b6e7e06fea34e58960edba082
Timm Mark Jensen <tje@simplesolution.dk>
2020-01-06 16:04:19 +0100

    Updated npm and composer files

--- header.php
+++ header.php
@@ -65,7 +65,7 @@
                     if(get_theme_mod( 'header_banner_title_setting' )){
                         echo get_theme_mod( 'header_banner_title_setting' );
                     }else{
-                        echo 'WordPress + Bootstrap';
+                        echo 'Simple Solution Boilerplate Theme';
                     }
                     ?>
                 </h1>
