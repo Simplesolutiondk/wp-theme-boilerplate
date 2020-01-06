bee8f9cafff51ced96d88d944b8b2d404e6b397e
Timm Mark Jensen <tje@simplesolution.dk>
2020-01-06 15:51:24 +0100

    Added Boostrap starter theme

--- header.php
+++ header.php
@@ -1,39 +1,88 @@
 <?php
 /**
  * The header for our theme
- **
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package WordPress
- * @subpackage sso-deco
- * @since 1.0.0
+ * @package WP_Bootstrap_Starter
  */
-?>
 
-<!doctype html>
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-        <link rel="profile" href="https://gmpg.org/xfn/11"/>
-	    <?php wp_enqueue_script("jquery"); ?>
-        <?php wp_head(); ?>
-        <script>
-            var currentTermId = <?php echo Helper::getCurrentTermId(); ?>;
-            var url = "<?php echo get_site_url(); ?>";
-            var urlSlug = "<?php echo Helper::getCurrentSlug(); ?>";
-            var postid = <?php if (get_the_ID()){ echo get_the_ID(); } else { echo 'false'; } ?>;
-            var posttype = <?php if (get_the_ID()){ echo '"'.get_post_type(get_the_ID()).'"'; } else { echo '""'; } ?>;
-            var is_logged_in = <?php echo is_user_logged_in() ? 'true' : 'false'; ?>;
-            var current_user = <?php echo get_current_user_id(); ?>;
-        </script>
-    </head>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+<?php wp_head(); ?>
+</head>
+
 <body <?php body_class(); ?>>
-    <header id="main-service">
-        <div id="header">
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sso-default' ); ?></a>
+    <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
+	<header id="masthead" class="site-header navbar-static-top <?php echo wp_bootstrap_starter_bg_class(); ?>" role="banner">
+        <div class="container">
+            <nav class="navbar navbar-expand-xl p-0">
+                <div class="navbar-brand">
+                    <?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
+                        <a href="<?php echo esc_url( home_url( '/' )); ?>">
+                            <img src="<?php echo esc_url(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+                        </a>
+                    <?php else : ?>
+                        <a class="site-title" href="<?php echo esc_url( home_url( '/' )); ?>"><?php esc_url(bloginfo('name')); ?></a>
+                    <?php endif; ?>
 
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <?php
+                wp_nav_menu(array(
+                'theme_location'    => 'primary',
+                'container'       => 'div',
+                'container_id'    => 'main-nav',
+                'container_class' => 'collapse navbar-collapse justify-content-end',
+                'menu_id'         => false,
+                'menu_class'      => 'navbar-nav',
+                'depth'           => 3,
+                'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+                'walker'          => new wp_bootstrap_navwalker()
+                ));
+                ?>
+
+            </nav>
         </div>
-    </header>
-    <header id="main-header">
-
-    </header>
+	</header><!-- #masthead -->
+    <?php if(is_front_page() && !get_theme_mod( 'header_banner_visibility' )): ?>
+        <div id="page-sub-header" <?php if(has_header_image()) { ?>style="background-image: url('<?php header_image(); ?>');" <?php } ?>>
+            <div class="container">
+                <h1>
+                    <?php
+                    if(get_theme_mod( 'header_banner_title_setting' )){
+                        echo get_theme_mod( 'header_banner_title_setting' );
+                    }else{
+                        echo 'WordPress + Bootstrap';
+                    }
+                    ?>
+                </h1>
+                <p>
+                    <?php
+                    if(get_theme_mod( 'header_banner_tagline_setting' )){
+                        echo get_theme_mod( 'header_banner_tagline_setting' );
+                }else{
+                        echo esc_html__('To customize the contents of this header banner and other elements of your site, go to Dashboard > Appearance > Customize','sso-default');
+                    }
+                    ?>
+                </p>
+                <a href="#content" class="page-scroller"><i class="fa fa-fw fa-angle-down"></i></a>
+            </div>
+        </div>
+    <?php endif; ?>
+	<div id="content" class="site-content">
+		<div class="container">
+			<div class="row">
+                <?php endif; ?>
