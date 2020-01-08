ea0b416b8d52bf67a3130aedbe128f4cbbbeb77b
Timm Mark Jensen <tje@simplesolution.dk>
2020-01-08 14:51:08 +0100

    Updated theme activation notice

--- functions.php
+++ functions.php
@@ -81,25 +81,25 @@ add_action( 'after_setup_theme', 'wp_bootstrap_starter_setup' );
 /**
  * Add Welcome message to dashboard
  */
-function wp_bootstrap_starter_reminder(){
-        $theme_page_url = 'https://afterimagedesigns.com/wp-bootstrap-starter/?dashboard=1';
+function sso_theme_activation_reminder(){
+    $theme_page_url = 'https://simplesolution.dk/';
 
-            if(!get_option( 'triggered_welcomet')){
-                $message = sprintf(__( 'Welcome to WP Bootstrap Starter Theme! Before diving in to your new theme, please visit the <a style="color: #fff; font-weight: bold;" href="%1$s" target="_blank">theme\'s</a> page for access to dozens of tips and in-depth tutorials.', 'sso-default' ),
-                    esc_url( $theme_page_url )
-                );
+        if(!get_option( 'triggered_welcomet')){
+            $message = sprintf(__( 'Thank you for using <strong>Simple Solution</strong> Boilerplate Theme, please visit <a style="color: #fff; font-weight: bold;" href="%1$s" target="_blank">simplesolution.dk</a>, for information and support.', 'sso-default' ),
+                esc_url( $theme_page_url )
+            );
 
-                printf(
-                    '<div class="notice is-dismissible" style="background-color: #6C2EB9; color: #fff; border-left: none;">
-                        <p>%1$s</p>
-                    </div>',
-                    $message
-                );
-                add_option( 'triggered_welcomet', '1', '', 'yes' );
-            }
+            printf(
+                '<div class="notice is-dismissible" style="background-color: #232427; color: #fff; border-left: none;">
+                    <p>%1$s</p>
+                </div>',
+                $message
+            );
+            add_option( 'triggered_welcomet', '1', '', 'yes' );
+        }
 
 }
-add_action( 'admin_notices', 'wp_bootstrap_starter_reminder' );
+add_action( 'admin_notices', 'sso_theme_activation_reminder' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
